aac2360f7740b6084e14e8cd2834eee9c9746a58
0mdsadiksadat <mdsadatsakib@gmail.com>
2023-08-22 23:41:13 +0600

    last line

---
+++ js/script.js
@@ -0,0 +1,48 @@
+document.getElementById('card-1').addEventListener('click',function(){
+    const product= document.getElementById('title-1');
+    const product1= product.innerText;
+    const box = document.getElementById('products-name');
+    const p = document.createElement('p');
+    p.innerText= product1;
+    box.appendChild(p);
+})
+document.getElementById('card-2').addEventListener('click',function(){
+    const product = document.getElementById('title-2');
+    const product2 = product.innerText;
+    const box = document.getElementById('products-name')
+    const p = document.createElement('p');
+    p.innerText= product2;
+    box.appendChild(p);
+})
+document.getElementById('card-3').addEventListener('click',function(){
+    const product = document.getElementById('title-3');
+    const product3 = product.innerText;
+    const box = document.getElementById('products-name')
+    const p = document.createElement('p');
+    p.innerText= product3;
+    box.appendChild(p);
+})
+document.getElementById('card-4').addEventListener('click',function(){
+    const product = document.getElementById('title-4');
+    const product4 = product.innerText;
+    const box = document.getElementById('products-name')
+    const p = document.createElement('p');
+    p.innerText= product4;
+    box.appendChild(p);
+})
+document.getElementById('card-5').addEventListener('click',function(){
+    const product = document.getElementById('title-5');
+    const product5 = product.innerText;
+    const box = document.getElementById('products-name')
+    const p = document.createElement('p');
+    p.innerText= product5;
+    box.appendChild(p);
+})
+document.getElementById('card-6').addEventListener('click',function(){
+    const product = document.getElementById('title-6');
+    const product6 = product.innerText;
+    const box = document.getElementById('products-name')
+    const p = document.createElement('p');
+    p.innerText= product6;
+    box.appendChild(p);
+})
